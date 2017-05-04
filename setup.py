7945dcd3b5491e6c76720eca7893ab74b860274e
lhotan <lhotan@2n.cz>
2017-05-04 13:17:43 +0200

    Added 2to3 option

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os, sys
 from setuptools import setup
 
-if not (sys.version_info.major==2 and sys.version_info.minor>=7):
+if not ((sys.version_info.major==2 and sys.version_info.minor>=7) or sys.version_info.major==3):
     sys.exit("Sorry, Python < 2.7 is not supported due to missing functionality in the built-in smtpd module")
 
 def read(fname):
@@ -16,6 +16,7 @@ setup(
     author_email="tintinweb@oststrom.com",
     description=("An extension to the standard python 2.x smtpd library implementing implicit/explicit SSL/TLS/STARTTLS"),
     license="",
+    use_2to3=True,
     keywords=["smtpd", "starttls", "tls", "ssl"],
     url="https://github.com/tintinweb/python-smtpd-tls",
     download_url="https://github.com/tintinweb/python-smtpd-tls/tarball/v0.1",
